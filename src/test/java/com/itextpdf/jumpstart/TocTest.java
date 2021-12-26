68a1a197d7d0e7fc5329b1dca84e6a985b9a5678
Peter Kratz <peterkratz@Peters-MacBook-Air.local>
2021-12-26 01:05:35 -0500

    Added test for page length for a valid Toc constructor

--- src/test/java/com/itextpdf/jumpstart/TocTest.java
+++ src/test/java/com/itextpdf/jumpstart/TocTest.java
@@ -1,9 +1,14 @@
 package com.itextpdf.jumpstart;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class TocTest {
-
+    
+    @Test
+    public void testValidToc() throws java.io.IOException{
+        Toc tocTest = new Toc("./input/toc.csv");
+        assertEquals(3, tocTest.tocPageLength);
+    }
 }
