13f49c9cdf4fda042a0cfcaa498279fb12ce7264
Peter Kratz <peterkratz@Peters-MacBook-Air.local>
2021-12-27 23:01:29 -0500

    added tests for deleteFile method

--- src/test/java/com/itextpdf/jumpstart/PdfUtilitiesTest.java
+++ src/test/java/com/itextpdf/jumpstart/PdfUtilitiesTest.java
@@ -1,9 +1,26 @@
 package com.itextpdf.jumpstart;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
-public class PdfUtilitiesTest {
+class PdfUtilitiesTest {
     
+    @Test
+    void deleteFileDoesNotThrowException() throws java.io.IOException {
+        String testFilePath = "./testing/testfile.txt";
+        File testFile = new File(testFilePath);
+        testFile.createNewFile();
+        PdfUtilities.deleteFile(testFilePath);
+        assertFalse(testFile.exists());
+    }
+
+    @Test
+    void deleteNonexistentFileThrowException() throws java.io.IOException {
+        assertThrows(java.io.IOException.class, () -> PdfUtilities.deleteFile("./testing/testdeletenotexist.txt"));
+    }
+
 }
