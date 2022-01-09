9e9d366ecc0426b689c749ba80c14091b5ccfdaf
Peter Kratz <peterkratz@Peters-MacBook-Air.local>
2022-01-09 15:34:17 -0500

    Uses JFileChooser to select input files and run AddToC. Still needs debugging

--- src/main/java/com/itextpdf/jumpstart/AddToc.java
+++ src/main/java/com/itextpdf/jumpstart/AddToc.java
@@ -7,7 +7,11 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Link;
 import com.itextpdf.layout.element.Paragraph;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class AddToc {
 
@@ -32,8 +36,22 @@ public class AddToc {
     //I have set up this main function to perform the tasks I need
     //In this case, it is to merge three PDFs and insert a Table of Contents from a CSV into a page number
     public static void main(String [] args) throws IOException {
+        JFileChooser csvChooser = new JFileChooser();
+        FileNameExtensionFilter csvFilter = new FileNameExtensionFilter("CSV for Table of Contents", "csv");
+        csvChooser.setFileFilter(csvFilter);
+        int csvReturnVal = csvChooser.showOpenDialog(null);
+        if(csvReturnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " + csvChooser.getSelectedFile().getName());
+        }
 
-        // Have left this empty - need to add implemention to let users select which files to use as pdf and csv
-
+        JFileChooser pdfChooser = new JFileChooser();
+        FileNameExtensionFilter pdfFilter = new FileNameExtensionFilter("PDF getting new table of contents", "pdf");
+        pdfChooser.setFileFilter(pdfFilter);
+        int pdfReturnVal = pdfChooser.showOpenDialog(null);
+        if(pdfReturnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: " + pdfChooser.getSelectedFile().getAbsolutePath());
+        }
+        Toc toc = new Toc(csvChooser.getSelectedFile().getAbsolutePath());
+        writeToc(pdfChooser.getSelectedFile().getAbsolutePath(), toc, "/Users/peterkratz/IdeaProjects/output/output.pdf");
     }
 }
